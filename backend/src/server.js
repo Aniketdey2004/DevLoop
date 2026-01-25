a7adc0521f3c543108830342026ff2f3a7963a8d
Aniket Dey <aniketdey2004@gmail.com>
2026-01-26 01:21:22 +0530

    deployment changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser"
 import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
@@ -12,9 +13,13 @@ import collabRoutes from "./routes/collab.route.js";
 import discussionRoutes from './routes/discussion.route.js';
 
 const app=express();
-const PORT=ENV.PORT || 8080;
+const PORT=ENV.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
+if(ENV.NODE_ENV!=='production'){
+    app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
+}
+
 app.use(express.json({limit:"10mb"}));
 app.use(cookieParser());
 app.use("/api/v1/auth",authRoutes); //authentication routes
@@ -24,6 +29,15 @@ app.use("/api/v1/notifications",notifyRoutes); //notification routes
 app.use("/api/v1/projects",projectRoutes);//project routes
 app.use("/api/v1/collab",collabRoutes); //collab Routes
 app.use("/api/v1/discussions",discussionRoutes);//discussion routes 
+
+if(ENV.NODE_ENV === "production")
+{
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(_,res)=>{
+        res.sendFile(path.resolve(__dirname,"../frontend","dist","index.html"));
+    });
+}
 
 app.listen(PORT,()=>{
     console.log(`App is listening on PORT ${PORT}`);

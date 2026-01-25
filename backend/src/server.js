d428e19d968727ed3f6d5f9fe0416e9095750361
Aniket Dey <aniketdey2004@gmail.com>
2026-01-26 01:49:30 +0530

    backend deployment fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,7 +34,7 @@ if(ENV.NODE_ENV === "production")
 {
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-    app.get("*",(_,res)=>{
+    app.get("(.*)",(_,res)=>{
         res.sendFile(path.resolve(__dirname,"../frontend","dist","index.html"));
     });
 }
